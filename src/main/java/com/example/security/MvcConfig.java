57ef2c45bf576f460cf07cc549197baf6f1120c1
Qocs <ahdehfl1004@gmail.com>
2024-07-26 17:45:57 +0900

    [fix comments of MvcConfig]

--- src/main/java/com/example/security/MvcConfig.java
+++ src/main/java/com/example/security/MvcConfig.java
@@ -9,9 +9,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
     //별도의 Controller 클래스 없이 특정 view에 대한 컨트롤러를 추가할 수 있는 메서드입니다.
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home"); //컨트롤러에서 home을 반환하면 /home 페이지를 보여줍니다.
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home"); // /home url이 요청되면 home이라는 view Object로 변환하여 home이라는 page로 이동합니다.
+        registry.addViewController("/").setViewName("home"); //시작 페이지를 /가 아닌 home으로.
         registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("login"); // /login url이 요청되면 login이라는 View object로 변환하여 이동하게 해줍니다.
     }
 }
