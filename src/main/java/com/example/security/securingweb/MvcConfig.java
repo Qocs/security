97ecc700a95dbbbfee8e4da01c8a71ddc1a93470
Qocs <ahdehfl1004@gmail.com>
2024-07-29 23:37:33 +0900

    [WebSecurityConfig]

--- src/main/java/com/example/security/securingweb/MvcConfig.java
+++ src/main/java/com/example/security/securingweb/MvcConfig.java
@@ -1,11 +1,11 @@
-package com.example.security;
+package com.example.security.securingweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer { //웹 애플리케이션은 Spring MVC를 기반으로 한다. 뷰 컨트롤러를 설정하기 위한 클래스
 
     //별도의 Controller 클래스 없이 특정 view에 대한 컨트롤러를 추가할 수 있는 메서드입니다.
     public void addViewControllers(ViewControllerRegistry registry) {
