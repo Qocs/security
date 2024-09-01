066fb621df83fe6d40c902a641c0e46adfbbc5f0
Qocs <ahdehfl1004@gmail.com>
2024-09-01 23:59:22 +0900

    JWTAuthenticationFilter before UsernamePasswordAuthenticationFilter

--- src/main/java/com/example/security/securingweb/SecurityConfig.java
+++ src/main/java/com/example/security/securingweb/SecurityConfig.java
@@ -12,6 +12,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/*
+ Spring Security 동작 설정 : Spring security는 여러 필터를 체인 형태로 연결하여 인증과 인가를 처리한다. 필터의 순서에 따라 인증 방식이 달라질 수 있다.
+ JWT 필터를 보안 체인에 추가
+    UsernamePasswordAuthenticationFilter의 역할 : 전통적인 폼 로그인 기반 로그인을 처리. 사용자명과 비밀번호를 이용한 인증 담당
+    JWT는 상태를 저장하지 않는 무상태 인증 방식이다. 모든 필요한 정보를 토큰에 담고 있다.
+    JWT 필터를 앞에 두는 이유는 JWT 토큰이 요청에 포함되어 있다면, 이미 인증된 사용자로 간주할 수 있다.
+    UsernamePassswordAuthenticationFilter 이전에 JWT 인증을 처리함으로써, 이미 인증된 사용자는 추가적인 인증 과정을 거치지 않아도 된다.
+ URL 별 접근 권한 설정
+ */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
